0a1fbc3514cdcfb461e606433bce4688dd72b2a2
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-21 15:31:07 -0400

    added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+
 import Header from "./components/Header/Header.js";
 function App() {
   return (
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,28 @@
+// import { a } from "react-router-dom";
 import "./Header.css";
 import watch from "../../assets/images/watch.mp4";
 
 export default function Header() {
   return (
-    <div className="video-container">
-      <h1 className="intro">WATCH - WORLD</h1>
-      <video className="video-bg" autoPlay muted loop>
-        <source src={watch} type="video/mp4" />
-      </video>
-    </div>
+    <>
+      <div className="video-container">
+        {/* Navigation */}
+        <nav>
+          <label>watch-world</label>
+          <ul>
+            <li>
+              <a href="google.com">Cart</a>
+            </li>
+            <li>
+              <a href="google.com">Wishlist</a>
+            </li>
+          </ul>
+        </nav>
+        {/* Navigation */}
+        <video className="video-bg" autoPlay muted loop>
+          <source src={watch} type="video/mp4" />
+        </video>
+      </div>
+    </>
   );
 }
