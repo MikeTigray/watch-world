9b39e4cbebf4cb4c4301d230d6f0ffa7f6ba9128
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-23 18:24:55 -0400

    added links to watch brands

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Landing />
-      {/* <Main /> */}
+      <Main />
       <Footer />
     </div>
   );
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,12 @@
 import "./Footer.css";
 import { AiOutlineArrowUp } from "react-icons/ai";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { TfiPinterest } from "react-icons/tfi";
+import { TfiTwitter } from "react-icons/tfi";
+
 export default function Footer() {
   return (
     <footer>
@@ -7,24 +14,102 @@ export default function Footer() {
         <div className="section-inner">
           <h3>Brands</h3>
           <ul>
-            <li>Fossil</li>
-            <li>Rolex</li>
-            <li>Casio</li>
-            <li>Citizen</li>
-            <li>Armani Exchange</li>
-            <li>Michael Kors</li>
-            <li>Gucci</li>
+            <li>
+              <a
+                href="https://www.fossil.com/en-us/ "
+                target="_blank"
+                rel="noreferrer"
+              >
+                Fossil
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.fossil.com/en-us/ "
+                target="_blank"
+                rel="noreferrer"
+              >
+                Rolex
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.casio.com/us/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Casio
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.citizenwatch.com/us/en/  "
+                target="_blank"
+                rel="noreferrer"
+              >
+                Citizen
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.armaniexchange.com/us/unisex/new-arrivals_section"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Armani Exchange
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.michaelkors.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Michael Kors
+              </a>
+            </li>
+            <li>
+              {" "}
+              <a
+                href="https://www.gucci.com/us/en/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Gucci
+              </a>
+            </li>
           </ul>
         </div>
         <div className="section-inner">
           <h3>Channels</h3>
           <ul>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Twitter</li>
-            <li>YouTube</li>
-            <li>Pinterest</li>
-            <li>LinkedIn</li>
+            <li>
+              <i>
+                <AiOutlineFacebook />
+              </i>
+              Facebook
+            </li>
+            <li>
+              <AiOutlineInstagram />
+              Instagram
+            </li>
+            <li>
+              <TfiTwitter /> Twitter
+            </li>
+            <li>
+              <AiOutlineYoutube /> YouTube
+            </li>
+            <li>
+              <TfiPinterest /> Pinterest
+            </li>
+            <li>
+              <AiOutlineLinkedin /> LinkedIn
+            </li>
           </ul>
         </div>
       </div>
