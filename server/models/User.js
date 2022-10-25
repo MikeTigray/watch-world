f387d1f751c245e88a3ba71fd436f129d16003a2
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-25 16:18:14 -0400

    modeled watch and user schemas and models

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+// const watchSchema = require("./Watch");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "A user must have a name"],
+    email: {
+      type: String,
+      required: [true, "A user must provide an email"],
+      match: [
+        /([a-zA-Z0-9-_\.]+)(@\w+\.)(\w{2,6})/,
+        "A user must provide a valid email address.",
+      ],
+      unique: [true, "A user must have a unique email"],
+    },
+  },
+  password: { type: String, required: true },
+  wishlist: { type: Schema.Types.ObjectId, ref: "Watch" },
+});
+const User = model("User", userSchema);
+
+module.exports = User;
--- server/models/Watch.js
+++ server/models/Watch.js
@@ -2,4 +2,9 @@ const { Schema, model } = require("mongoose");
 
 const watchSchema = new Schema({
   name: { type: String, required: true, unique: true },
+  price: { type: Number, required: true },
 });
+
+const Watch = model("Watch", watchSchema);
+
+module.exports = Watch;
