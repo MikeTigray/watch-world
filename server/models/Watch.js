445fc01b98510c0be2c8343808432f357f264930
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-29 17:56:58 -0400

    working on watch and user models

--- server/models/Watch.js
+++ server/models/Watch.js
@@ -3,6 +3,8 @@ const { Schema, model } = require("mongoose");
 const watchSchema = new Schema({
   name: { type: String, required: true, unique: true },
   price: { type: Number, required: true },
+  brand: { type: String, required: true },
+  image: { type: String },
 });
 
 const Watch = model("Watch", watchSchema);
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require("./User");
+const Watch = require("./Watch");
+
+module.exports = { User, Watch };
