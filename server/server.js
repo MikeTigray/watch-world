2cf736120d25eb61a2723a36b6be0aa20bb19973
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-29 22:18:26 -0400

    setting up apollo server, app and database connection in server.js

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`App listening at PORT: ${PORT} `);
+      console.log(
+        `Use Graphql at  http://localhost:${PORT}${server.graphqlPath} `
+      );
+    });
+  });
+};
+startApolloServer(typeDefs, resolvers);
