2a938df4dea88736d85b22064aab852670b2cf72
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-30 21:46:26 -0400

    added some more watch seeds

---
+++ server/seeders/seed.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    name: "Forrester Chronograph",
+    price: 150.0,
+    brand: "FOSSIL",
+    image: "",
+  },
+  {
+    name: "Limited Edition Harry Potter™",
+    price: 200.0,
+    brand: "FOSSIL",
+    image: "",
+  },
+  {
+    name: "Gen 6 Wellness",
+    price: 299.0,
+    brand: "FOSSIL",
+    image: "",
+  },
+];
