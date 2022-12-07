cd9dc8da090b507aec7c61f9b6be4359d7bea9af
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-12-07 18:20:53 -0500

    worked on payment cart's payment details

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BsTrash } from "react-icons/bs";
+import { BiDownArrow } from "react-icons/bi";
+import { BiUpArrow } from "react-icons/bi";
+
 import image from "../../assets/images/harry-potter.jpeg";
 import "./Cart.css";
 export default function Cart() {
+  const deleteItemFromCart = () => {};
+
   return (
     <div>
-      {/* Title */}
       <h2 className="shoppingtitle">Shopping Cart</h2>
-      {/* Container for watches and payment */}
       <div className="shoppinglist-container">
-        {/* Watches */}
-
         <div className="box">
+          {/*item  */}
           <div className="shoppinglist">
             <div className="inner-list-item">
               <div className="item-image-container">
@@ -20,15 +22,49 @@ export default function Cart() {
               <h2>harry potter watch</h2>
             </div>
             <div className="inner-list-item">
-              <div className="arrows"></div>
-              <BsTrash />
+              <h3>$650</h3>
+              <BsTrash onClick={deleteItemFromCart} />
             </div>
           </div>
+          {/*  */}
         </div>
 
         {/* Payment */}
         <div className="box">
-          <div className="payment-container">pay now</div>
+          <div className="payment-container">
+            <h3>Card Details</h3>
+            <form className="payment-submission">
+              <div className="payment-detail">
+                <label>Name on Card</label>
+                <input />
+              </div>
+              <div className="payment-detail">
+                <label>Card Number</label>
+                <input />
+              </div>
+              <div className="payment-detail">
+                <label>Expiration Date</label>
+                <input />
+              </div>
+
+              <div className="cost-detail">
+                <p>Subtotal</p>
+                <p>$650</p>
+              </div>
+              <div className="cost-detail">
+                <p>Shipping</p>
+                <p>$20</p>
+              </div>
+              <div className="cost-detail">
+                <p>Total</p>
+                <p>$670</p>
+              </div>
+              <button className="checkout-btn">
+                <p>$670</p>
+                <p>Checkout</p>
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
